969c47ab0702246e1d7e2bac56cf4e4f26d77c28
Donggeon Lee <dg981001@naver.com>
2023-06-29 17:34:03 +0900

    complete Fortran ver without MPI only

--- 1_data/3-1_data.f90
+++ 1_data/3-1_data.f90
@@ -1,18 +1,31 @@
-program sin_function
+program data_generator
   implicit none
-  real :: x, result
+  real(8) :: x, result,  start_, end_, step_, start_time, end_time, elapsed_
   integer :: i, n
   real, parameter :: pi = 3.1415926535897932384626433832795
+  real*8, external :: f1
   
+  call cpu_time(start_time)
+  start_ = 0.0
+  end_ = 100.0
+  step_ = 0.001
   ! 설정값
-  n = 1000  ! 구간을 몇 등분할 것인지 결정
-  
+  !n = INT((end_ - start_)/step_)  ! 구간을 몇 등분할 것인지 결정
+
+  result = 0.0
   ! 계산
-  do i = 0, n
-    x = 2.0 * pi * i / n
-    result = sin(x)
+  do x = start_, end_, step_ 
+    result = f1(x)
    !write(*, '(F8.4, F10.6)') x, result
   end do
   
-end program sin_function
+  call cpu_time(end_time)
 
+  elapsed_ = end_time - start_time
+
+  print *, elapsed_
+end program data_generator
+real(8) function f1(a)
+  real(8),intent(in) :: a
+  f1 = a**2 - 3 * a + 2
+end function f1
