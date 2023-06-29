f9f6d403ab84629b1e72aa1e1cb75e770a23d484
Donggeon Lee <dg981001@naver.com>
2023-06-29 17:33:17 +0900

    complete python ver

--- 1_data/1-1_data.py
+++ 1_data/1-1_data.py
@@ -1,22 +1,26 @@
-import numpy as np
+#import numpy as np
 import datetime
 start_time = datetime.datetime.now()
 
+# def f(x):
+#     #d, w0 = 1.5, 15
+#     #w = np.sqrt(w0**2-d**2)
+#     #phi = np.arctan(-d/w)
+#     #A = 1/(2*np.cos(phi))
+#     #cos = np.cos(phi+w*x)
+#     #sin = np.sin(phi+w*x)
+#     #exp = np.exp(-d*x)
+#     #y  = exp*2*A*cos
+#     y = np.pow(x, 2) - 3 * x + 2
+#     return y
+
 def f(x):
-    d, w0 = 1.5, 15
-    w = np.sqrt(w0**2-d**2)
-    phi = np.arctan(-d/w)
-    A = 1/(2*np.cos(phi))
-    cos = np.cos(phi+w*x)
-    sin = np.sin(phi+w*x)
-    exp = np.exp(-d*x)
-    y  = exp*2*A*cos
-    return y
+    return x**2 - 3*x + 2
 
 # Calculate and print f(x) for 0 < x < 4
 start = 0.0
-end = 4.0
-step = 0.1
+end = 10000.0
+step = 0.001
 
 x = start
 while x < end:
