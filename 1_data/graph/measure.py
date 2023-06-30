7e8e1567ae138e17583cc2581554944f75f0e3e9
johninkorea <john829829@gmail.com>
2023-06-30 12:18:20 +0900

    asd

--- 1_data/graph/measure.py
+++ 1_data/graph/measure.py
@@ -19,17 +19,19 @@ while z<len(asd):
     mad.append(np.median(time))
     lab.append(asd[z][:-8])
 
-    # plt.hist(time, bins=20, label=asd[z][:-7])
+    plt.hist(time, bins=1, label=asd[z][:-7])
     z+=1
 
 
-bins=np.linspace(0,3.4e6, 20)
-plt.hist(times, bins, label=[f'CUDA(1e5,1e2): {round(mad[0],2)}', f'python: {round(mad[1],2)}', f'python mpi(3): {round(mad[2],2)}', f'C++: {round(mad[3],2)}', f'C++ mpi(2): {round(mad[4],2)}'])
-
+# bins=np.linspace(0,3.4e6, 20)
+# plt.hist(times, bins, label=[f'CUDA(1e5,1e2): {round(mad[0],2)}', f'python: {round(mad[1],2)}', f'python mpi(3): {round(mad[2],2)}', f'C++: {round(mad[3],2)}', f'C++ mpi(2): {round(mad[4],2)}'])
+# plt.xscale('log')
+plt.xlabel(r'Time [$\mu$s]')# [log scale]')
+plt.ylabel('Count')
 # plt.legend()
 plt.savefig("asd", dpi=400)
 plt.show()
-
+print(mad)
 
 
 
